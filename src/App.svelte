7afab6fbadc6b2c909e660066ae297e03c2f176d
Josh Nussbaum <joshnuss@gmail.com>
2020-06-27 21:34:49 -0400

    Retabs files

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,69 @@
 <script>
-	import Equation from './Equation.svelte'
-	import animals from './animals.js'
-	import adjectives from './adjectives.js'
-	import { onMount } from 'svelte'
-	
-	const endings = [
-		"ify",
-		"ly",
-		"io"
-	]
-	const vowels = "aeiouy"
-	
-	let name = "cart"
-	let animal = animals[getRandomInt(animals.length)]
-	let ending = endings[getRandomInt(endings.length)]
-	let adjective = 'smart'
-	let first = 'accent', second = 'future'
-	let checked = []
-	
-	onMount(() => {
-		checked[5].checked = false
-		checked[6].checked = false
-		checked[7].checked = false
-	})
-	
-	$: {
-		checked = (first + second).split('').map(char => {
-			return {letter: char, checked: true}
-		})
-	}
-	$: value = checked.reduce((acc, char) =>  { return acc + (char.checked ? char.letter : '') }, '')
+  import Equation from './Equation.svelte'
+  import animals from './animals.js'
+  import adjectives from './adjectives.js'
+  import { onMount } from 'svelte'
 
-	
-	function refreshAnimal() {
-		const index = getRandomInt(animals.length)
-		animal = animals[index]
-	}
-	
-	function refreshEnding() {
-		const index = getRandomInt(endings.length)
-		ending = endings[index]
-	}
-	
-	function refreshAdjective() {
-		const index = getRandomInt(adjectives.length)
-		adjective = adjectives[index]
-	}
-	
-	function getRandomInt(max) {
-  	return Math.floor(Math.random() * Math.floor(max));
-	}
-	
-	function replaceEndings(a, b) {
-	  let replaced = a.replace(new RegExp(`${b}$`), "")
-	  if (replaced != a) return replaced
-	
-	  replaced = a.replace(new RegExp(`^${b}`), "")
-	  if (replaced != a) return replaced
-	
-	  return a.replace(new RegExp(b, 'g'), "")
-	}
-	
-	function toggleChecked(index) {
-		checked[index].checked = !checked[index].checked
-		checked = checked
-	}
+  const endings = [
+    "ify",
+    "ly",
+    "io"
+  ]
+  const vowels = "aeiouy"
+
+  let name = "cart"
+  let animal = animals[getRandomInt(animals.length)]
+  let ending = endings[getRandomInt(endings.length)]
+  let adjective = 'smart'
+  let first = 'accent', second = 'future'
+  let checked = []
+
+  onMount(() => {
+    checked[5].checked = false
+    checked[6].checked = false
+    checked[7].checked = false
+  })
+
+  $: {
+    checked = (first + second).split('').map(char => {
+      return {letter: char, checked: true}
+    })
+  }
+  $: value = checked.reduce((acc, char) =>  { return acc + (char.checked ? char.letter : '') }, '')
+
+  function refreshAnimal() {
+    const index = getRandomInt(animals.length)
+    animal = animals[index]
+  }
+
+  function refreshEnding() {
+    const index = getRandomInt(endings.length)
+    ending = endings[index]
+  }
+
+  function refreshAdjective() {
+    const index = getRandomInt(adjectives.length)
+    adjective = adjectives[index]
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  function replaceEndings(a, b) {
+    let replaced = a.replace(new RegExp(`${b}$`), "")
+    if (replaced != a) return replaced
+
+    replaced = a.replace(new RegExp(`^${b}`), "")
+    if (replaced != a) return replaced
+
+    return a.replace(new RegExp(b, 'g'), "")
+  }
+
+  function toggleChecked(index) {
+    checked[index].checked = !checked[index].checked
+    checked = checked
+  }
 </script>
 
 <!-- TODO add local storage store -->
@@ -72,11 +71,11 @@
 
 
 <h1>
-	SaaS Name Generator
+  SaaS Name Generator
 </h1>
 
 <Equation title="Animals" summary="Add an animal to the end of a name." bind:a={name} bind:b={animal} formula={(a, b) => `${a} ${b}`} showRefresh="b" on:refresh={refreshAnimal}/>
-	
+
 <Equation title="Appending" summary="Append a short ending to your name." bind:a={name} bind:b={ending} formula={(a, b) => `${a}${b}`} showRefresh="b" on:refresh={refreshEnding}/>
 
 <Equation title="Text Subtraction" summary="Subtract part of the name." bind:a={name} b={name[name.length-1]} op="-" formula={replaceEndings}/>
@@ -84,38 +83,38 @@
 <Equation title="Adjectives" summary="Add an adjective prefix to the name." bind:a={adjective} bind:b={name} formula={(a, b) => `${a} ${b}`} showRefresh="b" on:refresh={refreshAdjective}/>
 
 <Equation title="Joining" summary="Join multiple words together and remove some parts." bind:a={first} bind:b={second} formula={(a, b) => value}>
-	<div class="checklist">
-		{#each checked as char, index}
-			<label on:click={() => toggleChecked(index)} class:checked={char.checked}>
-				{char.letter}
-			</label>
-		{/each}
-	</div>
+  <div class="checklist">
+    {#each checked as char, index}
+      <label on:click={() => toggleChecked(index)} class:checked={char.checked}>
+        {char.letter}
+      </label>
+    {/each}
+  </div>
 </Equation>
 <Equation title="Removing vowels" summary="Remove one or more vowels from the name" bind:a={name} b={vowels} op="-" formula={(a, b) => a.replace(new RegExp(`${vowels.split('').join('|')}`, 'g'),"")}/>
 
 <footer>
-	By <a href="https://twitter.com/joshnuss">@joshnuss</a>
+  By <a href="https://twitter.com/joshnuss">@joshnuss</a>
 </footer>
-	
+
 <style>
-	a, h1 {
-		color: aqua;
-		text-decoration: underline;
-	}
-	.checklist label {
-		font-size: 2rem;
-		display: inline-block;
-		padding: 1px;
-		cursor: pointer;
-		background: red;
-		color: white;
-	}	
-	label.checked {
-		background: transparent;
-		color: black;
-	}
-	label.checked:hover {
-		text-decoration: underline;
-	}
+  a, h1 {
+    color: aqua;
+    text-decoration: underline;
+  }
+  .checklist label {
+    font-size: 2rem;
+    display: inline-block;
+    padding: 1px;
+    cursor: pointer;
+    background: red;
+    color: white;
+  }
+  label.checked {
+    background: transparent;
+    color: black;
+  }
+  label.checked:hover {
+    text-decoration: underline;
+  }
 </style>
--- src/Equation.svelte
+++ src/Equation.svelte
@@ -1,138 +1,137 @@
 <script>
-	import { createEventDispatcher } from 'svelte'
-	const dispatch = createEventDispatcher()
-	
-	export let title, summary
-	export let op = "+", a = "", b = "", formula = (a, b) => '???', showRefresh = null
-	
-	$: value = formula(a, b)
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+
+  export let title, summary
+  export let op = "+", a = "", b = "", formula = (a, b) => '???', showRefresh = null
+
+  $: value = formula(a, b)
 </script>
 
 <div class="card">
-	<h2>{title}</h2>
-	<p>{summary}</p>
-	<div class="container">
+  <h2>{title}</h2>
+  <p>{summary}</p>
+  <div class="container">
 
-	<div class="input-wrapper">
-		<input bind:value={a}/>	
-	
-		{#if showRefresh=="a"}
-		<button on:click|preventDefault={() => dispatch('refresh')}>
-			<svg width="14px" height="14px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-					<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-							<g id="refresh" fill="#000000">
-									<path d="M10,3 C6.13400675,3 3,6.13400675 3,10 C3,11.9329966 3.78350169,13.6829966 5.05025253,14.9497475 L6.46446609,13.5355339 C5.55964406,12.6307119 5,11.3807119 5,10 C5,7.23857625 7.23857625,5 10,5 L10,3 L10,3 Z M14.9497475,5.05025253 C16.2164983,6.31700338 17,8.06700338 17,10 C17,13.8659932 13.8659932,17 10,17 L10,15 C12.7614237,15 15,12.7614237 15,10 C15,8.61928813 14.4403559,7.36928813 13.5355339,6.46446609 L14.9497475,5.05025253 L14.9497475,5.05025253 Z M10,20 L6,16 L10,12 L10,20 L10,20 Z M10,8 L14,4 L10,0 L10,8 L10,8 Z" id="Combined-Shape"></path>
-							</g>
-					</g>
-			</svg>
-		</button>
-		{/if}
+  <div class="input-wrapper">
+    <input bind:value={a}/>
 
-	</div>
-  
-	<span class="op">
-		{op}
-	</span>
-
-	<div class="input-wrapper">
-		
-	<input bind:value={b}/>
-	
-	
-	{#if showRefresh=="b"}
-	<button on:click|preventDefault={() => dispatch('refresh')}>
-		<svg width="14px" height="14px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-				<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-						<g id="refresh" fill="#000000">
-								<path d="M10,3 C6.13400675,3 3,6.13400675 3,10 C3,11.9329966 3.78350169,13.6829966 5.05025253,14.9497475 L6.46446609,13.5355339 C5.55964406,12.6307119 5,11.3807119 5,10 C5,7.23857625 7.23857625,5 10,5 L10,3 L10,3 Z M14.9497475,5.05025253 C16.2164983,6.31700338 17,8.06700338 17,10 C17,13.8659932 13.8659932,17 10,17 L10,15 C12.7614237,15 15,12.7614237 15,10 C15,8.61928813 14.4403559,7.36928813 13.5355339,6.46446609 L14.9497475,5.05025253 L14.9497475,5.05025253 Z M10,20 L6,16 L10,12 L10,20 L10,20 Z M10,8 L14,4 L10,0 L10,8 L10,8 Z" id="Combined-Shape"></path>
-						</g>
-				</g>
-		</svg>
-	</button>
-	{/if}
+    {#if showRefresh=="a"}
+    <button on:click|preventDefault={() => dispatch('refresh')}>
+      <svg width="14px" height="14px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <g id="refresh" fill="#000000">
+                  <path d="M10,3 C6.13400675,3 3,6.13400675 3,10 C3,11.9329966 3.78350169,13.6829966 5.05025253,14.9497475 L6.46446609,13.5355339 C5.55964406,12.6307119 5,11.3807119 5,10 C5,7.23857625 7.23857625,5 10,5 L10,3 L10,3 Z M14.9497475,5.05025253 C16.2164983,6.31700338 17,8.06700338 17,10 C17,13.8659932 13.8659932,17 10,17 L10,15 C12.7614237,15 15,12.7614237 15,10 C15,8.61928813 14.4403559,7.36928813 13.5355339,6.46446609 L14.9497475,5.05025253 L14.9497475,5.05025253 Z M10,20 L6,16 L10,12 L10,20 L10,20 Z M10,8 L14,4 L10,0 L10,8 L10,8 Z" id="Combined-Shape"></path>
+              </g>
+          </g>
+      </svg>
+    </button>
+    {/if}
 
   </div>
-		
-		
-	<span class="op">
-		=
-	</span>
 
-	<span class="output">
-		<a href="https://domains.google.com/m/registrar/search?searchTerm={value}&tab=1" target="_blank">{ value }</a>
-		<slot/>
-	</span>	
-	</div>
+  <span class="op">
+    {op}
+  </span>
+
+  <div class="input-wrapper">
+
+  <input bind:value={b}/>
+
+
+  {#if showRefresh=="b"}
+  <button on:click|preventDefault={() => dispatch('refresh')}>
+    <svg width="14px" height="14px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="refresh" fill="#000000">
+                <path d="M10,3 C6.13400675,3 3,6.13400675 3,10 C3,11.9329966 3.78350169,13.6829966 5.05025253,14.9497475 L6.46446609,13.5355339 C5.55964406,12.6307119 5,11.3807119 5,10 C5,7.23857625 7.23857625,5 10,5 L10,3 L10,3 Z M14.9497475,5.05025253 C16.2164983,6.31700338 17,8.06700338 17,10 C17,13.8659932 13.8659932,17 10,17 L10,15 C12.7614237,15 15,12.7614237 15,10 C15,8.61928813 14.4403559,7.36928813 13.5355339,6.46446609 L14.9497475,5.05025253 L14.9497475,5.05025253 Z M10,20 L6,16 L10,12 L10,20 L10,20 Z M10,8 L14,4 L10,0 L10,8 L10,8 Z" id="Combined-Shape"></path>
+            </g>
+        </g>
+    </svg>
+  </button>
+  {/if}
+
+  </div>
+
+  <span class="op">
+    =
+  </span>
+
+  <span class="output">
+    <a href="https://domains.google.com/m/registrar/search?searchTerm={value}&tab=1" target="_blank">{ value }</a>
+    <slot/>
+  </span>
+  </div>
 </div>
 
 <style>
-	.card {
+  .card {
     background: #eee;
     padding: 1rem;
     margin-bottom: 13px;
     border-radius: 0.2em;
     box-shadow: 1px 1px #ddd;
-	}
-	
-	.container {
-		display: flex;
-	}
-	
-	.container > span {
-		padding: 0.2rem;
-	}
-	
-	.op, .output {
-		font-weight: bold;
-		color: #666;
+  }
+
+  .container {
+    display: flex;
+  }
+
+  .container > span {
+    padding: 0.2rem;
+  }
+
+  .op, .output {
+    font-weight: bold;
+    color: #666;
     align-self: center;
 
-	}
-	
-	.op {
-		margin: 0 0.5rem;
-	}
-	
-	
-	.output a {
-		color: aqua
-	}
-	
-	h2 {
-		flex: 0 0 100%;
-	}
-	
-	.input-wrapper {
-		border: solid 1px #ccc;
-		display: flex;
-		border-radius: 0.3rem;
-		background: #fff;
-	}
-	
-	.input-wrapper input {
-		width: 100%;
-		border: none;
-		background: none;	
-	}
-	
-	.input-wrapper input:focus {
-		outline: none;
-	}
-	.input-wrapper button {
-		flex: 1;
-		margin: none;
-	}
-	
-	button {
-		color: green;
-		background: none;
-		border: none;
-		cursor: pointer;
-	}
-	button:hover {
-		color: darkgreen;
-	}
-	svg path {
-		fill: currentColor
-	}
+  }
+
+  .op {
+    margin: 0 0.5rem;
+  }
+
+
+  .output a {
+    color: aqua
+  }
+
+  h2 {
+    flex: 0 0 100%;
+  }
+
+  .input-wrapper {
+    border: solid 1px #ccc;
+    display: flex;
+    border-radius: 0.3rem;
+    background: #fff;
+  }
+
+  .input-wrapper input {
+    width: 100%;
+    border: none;
+    background: none;
+  }
+
+  .input-wrapper input:focus {
+    outline: none;
+  }
+  .input-wrapper button {
+    flex: 1;
+    margin: none;
+  }
+
+  button {
+    color: green;
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+  button:hover {
+    color: darkgreen;
+  }
+  svg path {
+    fill: currentColor
+  }
 </style>
